b23b4c1a102a60e06e9da394b77f7e3592d3c45f
“pawn-01” <p47ingh@gmail.com>
2024-03-09 11:51:58 +0530

    new

--- Middleware.js
+++ Middleware.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const Middleware = (req,res,next) => {
     try{
     const { token } = req.cookies; 
-   // console.log(token);
+    console.log('Middleware  ' + token);
     if(!token || token==' ') return res.status(400).json({token:"token",a:-1});
     const decode = jwt.verify(token,process.env.Sercret_Key);
     //console.log(decode);
--- index.js
+++ index.js
@@ -15,7 +15,7 @@ require('dotenv').config()
 app.use(express.json());
 
 const corsOptions ={
-    origin:'https://blog-frontend-red-zeta.vercel.app', 
+    origin:`${process.env.Frontend_URL}`, 
     credentials:true,            //access-control-allow-credentials:true
     methods:['GET','POST','PUT','DELETE']
 }
